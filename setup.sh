ec6d6b2f1ab245528d50b1bcec0045f545afaed7
Kendrick Tan <kendricktan0814@gmail.com>
2016-12-31 18:19:14 +1000

    added zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt-get install i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh
+sudo apt-get install zsh curl i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp i3/* ~/.i3/
 cp vim/vimrc ~/.vimrc
