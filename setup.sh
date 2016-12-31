f2de88ab6ba4ddfe2cc21c32853b31bf94b6b34d
Kendrick Tan <kendricktan0814@gmail.com>
2016-12-31 18:05:22 +1000

    updated

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh
+
+cp i3/* ~/.i3/
+cp vim/vimrc ~/.vimrc
+cp xbindkeys/xbindkeysrc ~/.xbindkeysrc
+cp zsh/zshrc ~/.zshrc
