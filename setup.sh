9ede28a5487feebbb3d140b77e9f806bb17e3bb5
Kendrick Tan <kendricktan0814@gmail.com>
2017-01-02 12:47:29 +1000

    ycm -> jedi-vim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install zsh curl i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh cmake build-essential python-dev python3-dev
+sudo apt-get install zsh curl i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh cmake build-essential python-dev python3-dev vim-python-jedi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -8,8 +8,6 @@ cp i3/* ~/.i3/
 
 # vim plugins + YouCompleteMe
 cp vim/vimrc ~/.vimrc
-
-~/.vim/plugged/YouCompleteMe/install.py --clang-completer
 
 # bindkeys - volume, brightness etc
 cp xbindkeys/xbindkeysrc ~/.xbindkeysrc
