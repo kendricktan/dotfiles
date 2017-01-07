3456b85dc83ab8888d0e475425cc7b13f5af397f
Kendrick Tan <kendricktan0814@gmail.com>
2017-01-07 10:46:51 +1000

    ammended ssh script

--- scripts/start_gnome_ssh_keyring.sh
+++ scripts/start_gnome_ssh_keyring.sh
@@ -2,4 +2,5 @@
 
 eval $(ssh-agent -s)
 export $(gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
+export SSH_AGENT_PID=${GNOME_KEYRING_PID:-gnome}
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
