385980fc0a83d17eb0a083ae42440e0de8cc799a
Kendrick Tan <kendricktan0814@gmail.com>
2017-01-12 19:03:46 +1000

    updated zsh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 sudo apt-get install zsh curl i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh cmake build-essential python-dev python3-dev vim-python-jedi python3-pip haskell-platform
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Python
-sudo pip3 install virtualenvwrapper virtualenv
-
-# Haskell
-cabal update
-cabal install ghc-mod
 
 # i3
 mkdir -p ~/.i3/
@@ -29,3 +21,14 @@ sudo cp udev/99_trackpoint.rules /etc/udev/rules.d/99_trackpoint.rules
 
 # shell
 cp zsh/zshrc ~/.zshrc
+source ~/.zshrc
+
+# Python
+sudo pip3 install virtualenvwrapper virtualenv
+
+# Haskell
+curl -sSL https://get.haskellstack.org/ | sh # Stack
+cabal update
+cabal install ghc-mod
+stack update
+stack upgrade
