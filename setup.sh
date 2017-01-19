61fece8ce1e7c8653a9434a5543ca430f4b5ab24
Kendrick Tan <kendricktan0814@gmail.com>
2017-01-20 08:24:12 +1000

    rofi now fam

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-sudo apt-get install zsh curl i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh cmake build-essential python-dev python3-dev vim-python-jedi python3-pip haskell-platform
+sudo apt-get install zsh curl i3 xbindkeys xbacklight git vim vlc fonts-hack-ttf terminology screenfetch scrot feh cmake build-essential python-dev python3-dev vim-python-jedi python3-pip haskell-platform rofi zathura
+
+# zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # i3
 mkdir -p ~/.i3/
 cp i3/* ~/.i3/
 
-# vim plugins
+# vim settings
 cp vim/vimrc ~/.vimrc
 
 # bindkeys - volume, brightness, screenshot etc
