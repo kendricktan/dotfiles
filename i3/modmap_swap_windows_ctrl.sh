ab8431e499ceb0d9dc75fc7252b07ab34a1f23a3
Kendrick Tan <kendricktan0814@gmail.com>
2017-11-02 17:48:05 +1000

    ammend

---
+++ i3/modmap_swap_windows_ctrl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xmodmap -e "remove mod4 = Super_L"
+xmodmap -e "add control = Super_L"
---
+++ i3/popgun_screens.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+xrandr --output HDMI-1-1 --above eDP-1-1
+feh --bg-scale ~/Pictures/Wallpapers/magpie.png
